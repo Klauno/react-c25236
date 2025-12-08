27f22af272a052cb708033cb628ad6589da8530e
Clau <oliverioclau@gmail.com>
2025-12-07 21:40:43 -0300

    Actualizar LoadingSpinner con spinner lila estilizado

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,9 +1,14 @@
 import { Container, Spinner } from "react-bootstrap";
+import "./LoadingSpinner.css"; 
 
 export const LoadingSpinner = ({ text = "Cargando..." }) => {
   return (
     <Container className="py-5 text-center">
-      <Spinner animation="border" variant="primary" style={{ width: '5rem', height: '5rem',  borderWidth: '0.4rem' }}/>
+      <Spinner 
+        animation="border" 
+        className="spinner-lila"
+        style={{ width: '5rem', height: '5rem', borderWidth: '0.4rem' }}
+      />
       <p className="mt-2">{text}</p>
     </Container>
   );
